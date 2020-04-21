980b2ad30d069d02a4b04c41eff743ee79817bf1
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-04-21 10:13:21 +0200

    Added izziv6

---
+++ Izzivi/Izziv6/Izziv6/src/SeznamiUV.java
@@ -0,0 +1,125 @@
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class SeznamiUV {
+
+    HashMap<String, Seznam<String>> seznami;
+    Seznam<String> seznam;
+
+    public SeznamiUV() {
+        seznami = new HashMap<>();
+        seznami.put("pv", new PrioritetnaVrsta<>());
+        seznami.put("sk", new Sklad<>());
+        seznami.put("bst", new Bst<>());
+
+    }
+
+    public String processInput(String input) {
+        Scanner sc = new Scanner(input);
+        String token = sc.next();
+        String result = "OK", tmp;
+        try {
+            if (token.equals("use")) {
+                if (sc.hasNext()) {
+                    seznam = seznami.get(sc.next());
+                    if (null == seznam) {
+                        result = "Error: please specify a correct data structure type (pv, sk, bst)";
+                    }
+                } else {
+                    result = "Error: please specify a data structure type (pv, sk, bst)";
+                }
+            } else if (token.equals("add")) {
+                if (sc.hasNext()) {
+                    tmp = sc.next();
+//                        if (tmp.startsWith("\"")) {
+//                            tmp = tmp.substring(1);
+//                            while (sc.hasNext()) {
+//                                //tmp1=sc.next();
+//
+//                                tmp = tmp + " " + sc.next();
+//                                if (tmp.endsWith("\"")) {
+//                                    break;
+//                                }
+//                            }[
+//                            if (tmp.endsWith("\"")) {
+//                                tmp = tmp.substring(0, tmp.length() - 1);
+//                            } else {
+//                                result = "Error: please specify a correct string";
+//                                break;
+//                            }
+//                        }
+                    seznam.add(tmp);
+                } else {
+                    result = "Error: please specify a string";
+                }
+            } else if (token.equals("remove_first")) {
+                if (seznam.isEmpty()) {
+                    result = "Error: data structure is empty";
+                } else {
+                    result = seznam.removeFirst();
+                }
+
+            } else if (token.equals("size")) {
+                result = String.format("%d", seznam.size());
+            } else if (token.equals("get_first")) {
+                if (!seznam.isEmpty()) {
+                    result = seznam.getFirst();
+                } else {
+                    result = "Error: data structure is empty";
+                }
+            } else if (token.equals("depth")) {
+                return String.format("%d", seznam.depth());
+            } else if (token.equals("is_empty")) {
+                return String.format("Data structure is%s empty",
+                        seznam.isEmpty() ? "" : " not");
+            } else if (token.equals("exists")) {
+
+                if (sc.hasNext()) {
+                    if (seznam.isEmpty()) {
+                        result = "Data structure is empty";
+                    } else {
+                        result = "Element " + (seznam.exists(sc.next()) ? "exists " : "doesn't exist ") + "in data structure";
+                    }
+
+                } else {
+                    result = "Please specify a string";
+                }
+            } else if (token.equals("remove")) {
+                if (sc.hasNext()) {
+                    if (seznam.isEmpty()) {
+                        result = "Data structure is empty";
+                    } else {
+                        result = seznam.remove(sc.next());
+                    }
+
+                } else {
+                    result = "Error: please specify a string";
+                }
+            } else if (token.equals("reset")) {
+                if (seznam instanceof Sklad) {
+                    seznam = new Sklad<>();
+                } else if (seznam instanceof Bst) {
+                    seznam = new Bst<>();
+                } else {
+                    seznam = new PrioritetnaVrsta<>();
+                }
+
+            } else {
+                result = "Error: invalid command";
+            }
+
+//        } catch (UnsupportedOperationException e) {
+//            result = "Error: Operation not supported";
+        } catch (java.lang.IllegalArgumentException e) {
+            result = "Error: Duplicated entry";
+        } catch (java.util.NoSuchElementException e) {
+            result = "Error: element does not exist in data structure";
+        } catch (NullPointerException e) {
+            result = "Error: data structure is not selected";
+        }
+
+        return result;
+    }
+}
---
+++ Izzivi/Izziv6/Izziv6/src/Sklad.java
@@ -0,0 +1,123 @@
+
+class Element<Tip> {
+
+    public Tip vrednost;
+    public Element<Tip> vezava;
+
+    public Element(Tip e) {
+        this.vrednost = e;
+    }
+}
+
+public class Sklad<Tip> implements Seznam<Tip> {
+
+    private Element<Tip> vrh;
+
+    public Sklad() {
+    }
+
+    public void push(Tip e) {
+        Element<Tip> novVrh = new Element<>(e);
+        novVrh.vezava = vrh;
+        vrh = novVrh;
+    }
+
+    public Tip pop() {
+        if (vrh == null) {
+            throw new java.util.NoSuchElementException();
+        }
+        Tip e = vrh.vrednost;
+        vrh = vrh.vezava;
+        return e;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (vrh == null);
+    }
+
+    public Tip top() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+        return vrh.vrednost;
+    }
+
+    @Override
+    public int size() {
+        if (isEmpty()) {
+            return 0;
+        }
+        int stElementov = 0;
+        Element<Tip> tmp = vrh;
+        while (tmp != null) {
+            stElementov++;
+            tmp = tmp.vezava;
+        }
+        return stElementov;
+    }
+
+    public boolean isTop(Tip e) {
+        if (vrh == null) {
+            throw new java.util.NoSuchElementException();
+        }
+        return vrh.vrednost.equals(e);
+    }
+
+    public int search(Tip e) {
+        Element<Tip> tmp = vrh;
+        int i = 0;
+        while (null != tmp) {
+            if (tmp.vrednost.equals(e)) {
+                return i;
+            }
+            i++;
+            tmp = tmp.vezava;
+        }
+        return -1;
+    }
+
+    @Override
+    public void add(Tip e) {
+        push(e);
+    }
+
+    @Override
+    public Tip removeFirst() {
+        return pop();
+    }
+
+    @Override
+    public Tip getFirst() {
+        return top();
+    }
+
+    @Override
+    public int depth() {
+        return size();
+    }
+
+    @Override
+    public Tip remove(Tip e) {
+        Element<Tip> tmp = vrh;
+        Element<Tip> prejsni = null;
+        while (null != tmp) {
+            if (tmp.vrednost.equals(e)) {
+                if (prejsni == null) {
+                    vrh = vrh.vezava;
+                } else {
+                    prejsni.vezava = tmp;
+                }
+                return (Tip) tmp.vrednost;
+            }
+            prejsni = tmp;
+            tmp = tmp.vezava;
+        }
+        throw new java.util.NoSuchElementException();
+    }
+
+    @Override
+    public boolean exists(Tip e) {
+        return this.search(e) >= 0;
+    }
+}
